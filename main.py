fc2f330a75f2b5e3942e0c1fbd2d080fc6c7ac84
Farhandika <18220015@std.stei.itb.ac.id>
2022-11-23 09:49:27 +0700

    Added db

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI, Request
 from routes.prediction.predictionController import router as predictionRouter
 from services.machineLearning.machine_learning_service import ml_service
 from routes.auth.authController import router as authRouter
+from services.database.database_manager import conn
 
 ml_service.onCreateMLService()
 app = FastAPI()
@@ -16,16 +17,12 @@ async def root():
 
 @app.get("/teams")
 async def get_available_teams():
+    result = conn.execute("SELECT * FROM Teams")
+    teams = []
+    for team in result:
+        teams.append({"id": team[0], "name":team[1]})
     return {
-        "teams": [ 'Qatar', 'Senegal', 'Netherlands', 'Ecuador',
-                     'England', 'USA', 'Iran', 'Wales',
-                     'Argentina', 'Mexico', 'Poland','Saudi Arabia',
-                     'France', 'Denmark', 'Tunisia', 'Australia',
-                     'Japan', 'Spain', 'Germany', 'Costa Rica',
-                     'Belgium', 'Croatia', 'Morocco', 'Canada',
-                     'Brazil', 'Cameroon', 'Potter', 'Switzerland',
-                     'Ghana', 'Uruguay', 'Korea Republic', 'Portugal'
-                     ]
+        "teams": teams
     }
 
 
